54e4769a36f1db2f3bf92718adf146ac27cde0ba
cracklegend <enquiry.cracklegend@gmail.com>
2016-12-26 04:48:59 +0800

    Update fetch-view-count.js

--- fetch-view-count.js
+++ fetch-view-count.js
@@ -1,5 +1,4 @@
 var data_blog_view_count;
-console.log(data_blog_view_count);
 data_blog_view_count = $('span#Stats1_totalCount.counter-wrapper.text-counter-wrapper').text();
 $('li#view-count.view-count').text(data_blog_view_count);
 if (typeof myFunction1 == 'function') {
