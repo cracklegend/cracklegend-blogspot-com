dd1121a5d019467ab2c658b5f629069a139f4183
cracklegend <enquiry.cracklegend@gmail.com>
2016-12-26 04:40:22 +0800

    Create right-panel.js

---
+++ right-panel.js
@@ -0,0 +1,30 @@
+$('div.htdlb').children('div.htdl').hide(333);
+$('div.htdlb').hover( function(){
+  $(this).children('div.readmore').hide(333);
+  $('div.htdl').show(333);
+}, function(){
+  $(this).children('div.readmore').show(333);
+  $('div.htdl').hide(333);
+})
+
+
+
+$('div.creqb').children('div.creq').hide(333);
+$('div.creqb').hover( function(){
+  $(this).children('div.readmore').hide(333);
+  $('div.creq').show(333);
+}, function(){
+  $(this).children('div.readmore').show(333);
+  $('div.creq').hide(333);
+})
+
+
+
+$('div.smb').children('div.sm').hide(333);
+$('div.smb').hover( function(){
+  $(this).children('div.readmore').hide(333);
+  $('div.sm').show(333);
+}, function(){
+  $(this).children('div.readmore').show(333);
+  $('div.sm').hide(333);
+})
