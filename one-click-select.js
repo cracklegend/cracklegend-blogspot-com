15d79b31f5ec50c2d8d23e8ba22e675f4ee2628f
cracklegend <enquiry.cracklegend@gmail.com>
2016-12-26 04:30:10 +0800

    Create one-click-select.js

---
+++ one-click-select.js
@@ -0,0 +1,17 @@
+$.fn.OneClickSelect = function() {
+  return $(this).on('click', function() {
+    var range, selection;
+    if (window.getSelection) {
+      selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents(this);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    } else if (document.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText(this);
+      range.select();
+    }
+  });
+};
+$('kbd').OneClickSelect();
