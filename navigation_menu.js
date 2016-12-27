2bc534d3d036cdb87e17bc8e0261eb4a73b82415
cracklegend <enquiry.cracklegend@gmail.com>
2016-12-27 13:46:45 +0800

    Update navigation_menu.js

--- navigation_menu.js
+++ navigation_menu.js
@@ -1,181 +1,111 @@
 var cl_navigation_menu = `<style>
-table.navmenuwv2 {
-  /*position: absolute;*/
-}
-table.navmenuwv2 td.rimg {
-  padding: 0;
-  background-color: rgba(203, 227, 255, 0.3125);
-  border-collapse: separate;
-  border-radius: 40px;
-  border: 2px solid #E0E0FF;
-  white-space: nowrap;
+td.rimg {
   width: 166px;
+  text-align: center;
+  border-radius: 40px;
 }
-table.navmenuwv2 td.rimg div#rotate {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-table.navmenuv2 td {
-  padding: 0;
-  background-color: rgba(203, 227, 255, 0.3125);
+table.navmenuv3 {
   border-collapse: separate;
-  border-radius: 50px;
+}
+table.navmenuv3 td, td.rimg {
   border: 2px solid #E0E0FF;
-  white-space: nowrap;
-  font-size: 40px;
-  font-weight: bold;
-  width: 74px;
+  padding: 0;
+  background-color: rgba(203, 227, 255, 0.3125);}
+table.navmenuv3 td {
+  border-radius: 50px;
+  min-width: 74px;
+  max-width: 74px;
   overflow: hidden;
+  white-space: nowrap;
   -webkit-transition: all .5s;
   -moz-transition: all .5s;
   -o-transition: all .5s;
   transition: all .5s;
-  -webkit-transition-timing-function: ease-in-out;
-  -moz-transition-timing-function: ease-in-out;
-  -o-transition-timing-function: ease-in-out;
-  transition-timing-function: ease-in-out;
+  -webkit-transition-timing-function: ease-out;
+  -moz-transition-timing-function: ease-out;
+  -o-transition-timing-function: ease-out;
+  transition-timing-function: ease-out;
 }
-table.navmenuv2 td.l1:hover {
-  width: 180px;
+table.navmenuv3 td:not(.l1):hover {
+  max-width: 280px;
 }
-table.navmenuv2 td.l2:hover {
-  width: 260px;
+table.navmenuv3 td.l1:hover {
+  max-width: 180px;
 }
-table.navmenuv2 div {
-  position: relative;
-  display: flex;
-  align-items: center;
-  width: 100%;
+table.navmenuv3 td > * {
+  vertical-align: middle;
 }
-table.navmenuv2 td img {
-  width: 64px;
+table.navmenuv3 img {
   padding: 5px;
+  width: 64px;
+  height: 64px;
 }
-table.navmenuv2 div div {
-  position: relative;
-}
-table.navmenuv2 div div div {
-  padding-left: 4px;
-  position: absolute;
+table.navmenuv3 span {
+  font-weight: bold;
+  padding-right: 20px;
+  font-size: 40px;
   color: #EFEFFF;
   text-shadow: 1px 1px 4px #6666FF, 2px 2px 8px #4747FF;
 }
 </style>
-<table class='exc navmenuwv2' style='margin-top: 15px; margin-bottom: 15px; position: relative; left: -2px;'>
-  <tr>
-    <td class='null rimg' rowspan='2'>
-      <div id='rotate'><img alt='Crack Legend' height='128px' src='http://i.imgur.com/IvtMwTp.png' title='Crack Legend' width='128px' /></div>
-    </td>
-    <td class='null'>
-      <table class='exc navmenuv2'>
-        <tr>
-          <td class='null l1' onclick='window.document.location="http://cracklegend.blogspot.com" '>
-            <div>
-              <img src='http://icons.iconarchive.com/icons/graphicloads/100-flat/128/home-icon.png' />
-              <div>
-                <div>主頁</div>
-              </div>
-            </div>
-          </td>
-          <td class='null l2' onclick='window.document.location="http://cracklegend.blogspot.com/p/hot.html" '>
-            <div>
-              <img src='http://i.imgur.com/QNnhBRz.png' />
-              <div>
-                <div>熱門文章</div>
-              </div>
-            </div>
-          </td>
-          <td class='null l2' onclick='window.document.location="http://cracklegend.blogspot.com/p/exclusive.html" '>
-            <div>
-              <img src='http://i.imgur.com/DPpmSas.png' />
-              <div>
-                <div>獨家文章</div>
-              </div>
-            </div>
-          </td>
-          <td class='null l2' onclick='window.document.location="http://cracklegend.blogspot.com/p/games.html" '>
-            <div>
-              <img src='http://i.imgur.com/51k2FoE.png' />
-              <div>
-                <div>遊戲分享</div>
-              </div>
-            </div>
-          </td>
-          <td class='null l2' onclick='window.document.location="http://cracklegend.blogspot.com/p/tutorials.html" '>
-            <div>
-              <img src='http://i.imgur.com/nFW4ulP.png' />
-              <div>
-                <div>心得分享</div>
-              </div>
-            </div>
-          </td>
-          <td class='null l2' onclick='window.document.location="http://cracklegend.blogspot.com/p/status.html" '>
-            <div>
-              <img src='http://i.imgur.com/5MCTpIl.png' />
-              <div>
-                <div>網誌狀況</div>
-              </div>
-            </div>
-          </td>
-        </tr>
-      </table>
-    </td>
-  </tr>
-  <tr>
-    <td class='null'>
-      <table class='exc navmenuv2'>
-        <tr>
-          <td class='null l2' onclick='window.document.location="http://cracklegend.blogspot.com/p/disclaimer.html" '>
-            <div>
-              <img src='http://i.imgur.com/djQl9rv.png' />
-              <div>
-                <div>免責聲明</div>
-              </div>
-            </div>
-          </td>
-          <td class='null l2' onclick='window.document.location="http://cracklegend.blogspot.com/p/dmca.html" '>
-            <div>
-              <img src='http://i.imgur.com/LkRyvVE.png' />
-              <div>
-                <div>ＤＭＣＡ</div>
-              </div>
-            </div>
-          </td>
-          <td class='null l2' onclick='window.document.location="http://cracklegend.blogspot.com/p/creative-commons.html" '>
-            <div>
-              <img src='http://i.imgur.com/7MPAOoz.png' />
-              <div>
-                <div>創用ＣＣ</div>
-              </div>
-            </div>
-          </td>
-          <td class='null l2' onclick='window.document.location="http://cracklegend.blogspot.com/p/about-author.html#donate" '>
-            <div>
-              <img src='http://i.imgur.com/2WX6cJo.png' />
-              <div>
-                <div>捐贈贊助</div>
-              </div>
-            </div>
-          </td>
-          <td class='null l2' onclick='window.document.location="http://cracklegend.blogspot.com/p/friend-links.html" '>
-            <div>
-              <img src='http://i.imgur.com/lVfjUya.png' />
-              <div>
-                <div>友情連結</div>
-              </div>
-            </div>
-          </td>
-          <td class='null l2' onclick='window.document.location="http://cracklegend.blogspot.com/p/about-author.html" '>
-            <div>
-              <img src='http://i.imgur.com/iWeFPDU.png' />
-              <div>
-                <div>關於編輯</div>
-              </div>
-            </div>
-          </td>
-        </tr>
-      </table>
-    </td>
-  </tr>
+<table class='exc nmwv3' style='margin-top: 15px; margin-bottom: 15px; position: relative; left: -2px;'>
+<tr>
+<td class='null rimg' rowspan='2'>
+<div id='rotate'><img alt='Crack Legend' height='128px' src='http://i.imgur.com/IvtMwTp.png' title='Crack Legend' width='128px' /></div>
+</td>
+<td class='null'>
+
+<table class='exc navmenuv3'>
+<tr>
+<td class='null l1' onclick='window.document.location="http://cracklegend.blogspot.com" '>
+<img src='http://icons.iconarchive.com/icons/graphicloads/100-flat/128/home-icon.png' /><span>主頁</span>
+</td>
+<td class='null' onclick='window.document.location="http://cracklegend.blogspot.com/p/hot.html" '>
+<img src='http://i.imgur.com/QNnhBRz.png' /><span>熱門文章</span>
+</td>
+<td class='null' onclick='window.document.location="http://cracklegend.blogspot.com/p/exclusive.html" '>
+<img src='http://i.imgur.com/DPpmSas.png' /><span>獨家文章</span>
+</td>
+<td class='null' onclick='window.document.location="http://cracklegend.blogspot.com/p/games.html" '>
+<img src='http://i.imgur.com/51k2FoE.png' /><span>遊戲分享</span>
+</td>
+<td class='null' onclick='window.document.location="http://cracklegend.blogspot.com/p/tutorials.html" '>
+<img src='http://i.imgur.com/nFW4ulP.png' /><span>心得分享</span>
+</td>
+<td class='null' onclick='window.document.location="http://cracklegend.blogspot.com/p/status.html" '>
+<img src='http://i.imgur.com/5MCTpIl.png' /><span>網誌狀況</span>
+</td>
+</tr>
+</table>
+
+</td>
+</tr>
+<tr>
+<td class='null'>
+
+<table class='exc navmenuv3'>
+<tr>
+<td class='null' onclick='window.document.location="http://cracklegend.blogspot.com/p/disclaimer.html" '>
+<img src='http://i.imgur.com/djQl9rv.png' /><span>免責聲明</span>
+</td>
+<td class='null' onclick='window.document.location="http://cracklegend.blogspot.com/p/dmca.html" '>
+<img src='http://i.imgur.com/LkRyvVE.png' /><span>ＤＭＣＡ</span>
+</td>
+<td class='null' onclick='window.document.location="http://cracklegend.blogspot.com/p/creative-commons.html" '>
+<img src='http://i.imgur.com/7MPAOoz.png' /><span>創用ＣＣ</span>
+</td>
+<td class='null' onclick='window.document.location="http://cracklegend.blogspot.com/p/about-author.html#donate" '>
+<img src='http://i.imgur.com/2WX6cJo.png' /><span>捐贈贊助</span>
+</td>
+<td class='null' onclick='window.document.location="http://cracklegend.blogspot.com/p/friend-links.html" '>
+<img src='http://i.imgur.com/lVfjUya.png' /><span>友情連結</span>
+</td>
+<td class='null' onclick='window.document.location="http://cracklegend.blogspot.com/p/about-author.html" '>
+<img src='http://i.imgur.com/iWeFPDU.png' /><span>關於編輯</span>
+</td>
+</tr>
+</table>
+  
+</td>
+</tr>
 </table>`;
